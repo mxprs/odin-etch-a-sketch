7569a0401d5fcec8aa7a2aa7b422063a69fd6ed1
mxprs <seriousgrowthtalk@gmail.com>
2022-02-01 23:02:42 +0100

    ongoing - reset button

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const container = document.querySelector('#container');
 
-for (let index = 0; index < 16*16; index++) {
-    let newNode = document.createElement('div');
-    newNode.classList = 'grid-item inactive-pixel';
-    newNode.textContent = index;
-    container.appendChild(newNode);
-    
+function createNewGrid (gridSize) {
+    for (let index = 0; index < gridSize*gridSize; index++) {
+        let newNode = document.createElement('div');
+        newNode.classList = 'grid-item inactive-pixel';
+        newNode.textContent = index;
+        container.style.cssText = `grid-template-rows: repeat(${gridSize}, 50px); grid-template-columns: repeat(${gridSize}, 1fr);`;
+        container.appendChild(newNode);
+    }
 }
+
 
 const grid = document.querySelectorAll('.grid-item');
 
@@ -19,3 +22,13 @@ const btnErase = document.querySelector('.erase');
 btnErase.addEventListener('click', function() {
     grid.forEach(element => element.classList = 'grid-item inactive-pixel');
 });
+
+const btnReset = document.querySelector('.reset');
+btnReset.addEventListener('click', function() {
+    grid.forEach(element => element.remove());
+    createNewGrid(16);
+    // solve if clicked twice - grid is two times
+    // grid not reacting on mouse over anymore....
+    // continue grinding the exercise
+})
+
