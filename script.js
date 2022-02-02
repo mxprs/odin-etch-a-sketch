80fc90ba61b028d01be507a636c05c0e04e36696
mxprs <seriousgrowthtalk@gmail.com>
2022-02-02 15:34:25 +0100

    implemented reset feature with correction of last bugs

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
-const container = document.querySelector('#container');
 
-function createNewGrid (gridSize) {
-    for (let index = 0; index < gridSize*gridSize; index++) {
+//TODO - solve if clicked twice - grid is two times
+//TODO - grid not reacting on mouse over anymore....
+//TODO - continue grinding the exercise
+
+function createNewGrid(size) {
+    const container = document.createElement('div');
+    container.setAttribute("id", "container");
+    document.querySelector('body').appendChild(container);
+
+    for (let index = 0; index < size*size; index++) {
         let newNode = document.createElement('div');
         newNode.classList = 'grid-item inactive-pixel';
-        newNode.textContent = index;
-        container.style.cssText = `grid-template-rows: repeat(${gridSize}, 50px); grid-template-columns: repeat(${gridSize}, 1fr);`;
+        container.style.cssText = `grid-template-rows: repeat(${size}, 1fr); grid-template-columns: repeat(${size}, 1fr);`;
         container.appendChild(newNode);
     }
+
+    gridAnimationMouseOver()
+
 }
 
+function gridAnimationMouseOver() {
+    const grid = document.querySelectorAll('.grid-item');
 
-const grid = document.querySelectorAll('.grid-item');
+    grid.forEach(element => element.addEventListener('mouseover', (e)=> {
+        e.target.classList.remove("inactive-pixel");
+        e.target.classList.add("active-pixel");
+    }));
 
-grid.forEach(element => element.addEventListener('mouseover', (e)=> {
-    e.target.classList.remove("inactive-pixel");
-    e.target.classList.add("active-pixel");
-}));
+    const btnErase = document.querySelector('.erase');
+    btnErase.addEventListener('click', function() {
+        grid.forEach(element => element.classList = 'grid-item inactive-pixel');
+    });
+}
 
-const btnErase = document.querySelector('.erase');
-btnErase.addEventListener('click', function() {
-    grid.forEach(element => element.classList = 'grid-item inactive-pixel');
-});
+// Init
+
+createNewGrid(16);
+
+// Runtime
+
+
 
 const btnReset = document.querySelector('.reset');
 btnReset.addEventListener('click', function() {
-    grid.forEach(element => element.remove());
-    createNewGrid(16);
-    // solve if clicked twice - grid is two times
-    // grid not reacting on mouse over anymore....
-    // continue grinding the exercise
+    container.remove();
+    let gridSize = prompt("What size would you like?");
+    createNewGrid(gridSize);
+
 })
 
