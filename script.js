9c207f16e256470e16fd291e6907f5770eeaf44f
mxprs <seriousgrowthtalk@gmail.com>
2022-02-02 15:54:38 +0100

    general code clean up

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+// ------ 0/ Logic
 
-//TODO - solve if clicked twice - grid is two times
-//TODO - grid not reacting on mouse over anymore....
-//TODO - continue grinding the exercise
-
+// function to create a new grid with animation on event mouse over
 function createNewGrid(size) {
     const container = document.createElement('div');
     container.setAttribute("id", "container");
     document.querySelector('body').appendChild(container);
 
+    // generate the grid on the given size
     for (let index = 0; index < size*size; index++) {
         let newNode = document.createElement('div');
         newNode.classList = 'grid-item inactive-pixel';
@@ -15,37 +14,57 @@ function createNewGrid(size) {
         container.appendChild(newNode);
     }
 
+    // Add the animation on the grid on mouse over event
     gridAnimationMouseOver()
+
+    // add info below the grid to notify user of the size of the grid
+    const textBlock = document.createElement('p');
+    textBlock.textContent = `Current size is ${size}x${size}.`;
+    textBlock.className = 'info-size-grid';
+    document.querySelector('body').appendChild(textBlock);
 
 }
 
+// function for the animation mouse over
 function gridAnimationMouseOver() {
     const grid = document.querySelectorAll('.grid-item');
 
+    // change each div to active if mouseovered
     grid.forEach(element => element.addEventListener('mouseover', (e)=> {
         e.target.classList.remove("inactive-pixel");
         e.target.classList.add("active-pixel");
     }));
 
+    // Feature of the clean button with a click listener
     const btnErase = document.querySelector('.erase');
     btnErase.addEventListener('click', function() {
         grid.forEach(element => element.classList = 'grid-item inactive-pixel');
     });
 }
 
-// Init
+// ------ 1/ Init
 
-createNewGrid(16);
+// Initialize the grid at first visit
+createNewGrid(30);
 
-// Runtime
+// ------ 2/ Runtime
 
-
-
+// Feature of the reset button with a click listener
 const btnReset = document.querySelector('.reset');
 btnReset.addEventListener('click', function() {
+
+    // delete the info 'p' element for the size of the grid
+    document.querySelector('.info-size-grid').remove();
+
+    // remove the container with the grid inside
     container.remove();
+
+    // request which size the user wants
     let gridSize = prompt("What size would you like?");
+
+    // generate the new grid
     createNewGrid(gridSize);
+    
 
 })
 
